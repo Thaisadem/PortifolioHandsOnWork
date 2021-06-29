9c03764887e8bcfb2342811aad21922ca6280ccf
Thaisa <thaisa.medeiros@ead.univali.br>
2021-06-29 19:21:01 -0300

    master

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Personalização do slider para exibição das imagens no topo do site
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
